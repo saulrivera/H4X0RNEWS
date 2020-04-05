637cf0bd2653367fd6be932e53165ddc7a2388a6
Saul Rivera <saulriveraal@icloud.com>
2020-04-05 00:18:25 -0600

    Initial Commit

---
+++ H4X0R News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Saul Rivera on 05/04/20.
+//  Copyright © 2020 Saul Rivera. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
